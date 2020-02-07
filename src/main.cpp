e631992354371b01680fea681451a3c8207c7b92
Florian Meinicke <florian.meinicke@t-online.de>
2020-02-07 16:09:44 +0100

    Fix documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,9 @@
-/******************************************************************************
- ** Copyright (c) 2020 Florian Meinicke <florian.meinicke@t-online.de>.
- **
- ** This program is free software: you can redistribute it and/or modify
- ** it under the terms of the GNU Lesser General Public License as
- ** published by the Free Software Foundation, either version 3 of the
- ** License, or (at your option) any later version.
- **
- ** This program is distributed in the hope that it will be useful,
- ** but WITHOUT ANY WARRANTY; without even the implied warranty of
- ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- ** GNU Lesser General Public License for more details.
- **
- ** You should have received a copy of the GNU Lesser General Public License
- ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *****************************************************************************/
-
 //============================================================================
-/// \file   %{Cpp:License:FileName}
+/// \file   main.cpp
 /// \author Florian Meinicke <florian.meinicke@t-online.de>
 /// \date   06/02/2020
-/// \brief  Implementation Declaration of the %{Cpp:License:ClassName} class.
+/// \brief  A mobile app that displays the weather forecast from a public
+/// REST-API
 //============================================================================
 
 //============================================================================
