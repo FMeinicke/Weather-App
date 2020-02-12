d186afeeaaffcf80b7c1dd3f5e5364adb6bd2cef
Florian Meinicke <florian.meinicke@t-online.de>
2020-02-12 18:19:38 +0100

    Add slot to search for a given location

--- src/WeatherApi.cpp
+++ src/WeatherApi.cpp
@@ -11,11 +11,17 @@
 #include "WeatherApi.h"
 
 #include <QDebug>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QNetworkRequest>
 
 using namespace std;
+
+static const auto BASE_URL =
+    QStringLiteral("https://www.metaweather.com/api/location/");
 
 //=============================================================================
 CWeatherApi::CWeatherApi(QObject* parent)
@@ -39,10 +45,47 @@ void CWeatherApi::requestData()
         m_DataBuffer = m_NetReply->readAll();
         emit dataReady(m_DataBuffer);
     });
-    connect(m_NetReply.get(), &QNetworkReply::finished, this, [this]() {
-        // clean up
-        m_NetReply->deleteLater();
-        m_NetReply = nullptr;
-        m_DataBuffer.clear();
+    connect(m_NetReply.get(), &QNetworkReply::finished, this,
+            &CWeatherApi::cleanUp);
+}
+
+//=============================================================================
+void CWeatherApi::requestLocation(const QString& desiredLocation)
+{
+    const auto Request =
+        QNetworkRequest{BASE_URL + "search/?query=" + desiredLocation};
+    m_NetReply.reset(m_NetAccessManager->get(Request));
+
+    connect(m_NetReply.get(), &QNetworkReply::readyRead, this, [this]() {
+        m_DataBuffer = m_NetReply->readAll();
+        QJsonParseError Error;
+        const auto ResponseJsonDoc =
+            QJsonDocument::fromJson(m_DataBuffer, &Error);
+        if (ResponseJsonDoc.isNull())
+        {
+            qWarning() << "Could not parse received JSON data! The following "
+                          "error occured:"
+                       << Error.errorString();
+        }
+
+        const auto LocationsJsonArray = ResponseJsonDoc.array();
+
+        auto Locations = QStringList();
+        for_each(begin(LocationsJsonArray), end(LocationsJsonArray),
+                 [&Locations](const QJsonValue& val) {
+                     const auto Location = val.toObject();
+                     Locations.append(Location["title"].toString());
+                  });
+        emit locationsReady(Locations);
     });
+    connect(m_NetReply.get(), &QNetworkReply::finished, this,
+            &CWeatherApi::cleanUp);
+}
+
+//=============================================================================
+void CWeatherApi::cleanUp()
+{
+    m_NetReply->deleteLater();
+    m_NetReply = nullptr;
+    m_DataBuffer.clear();
 }
--- src/WeatherApi.h
+++ src/WeatherApi.h
@@ -50,11 +50,36 @@ signals:
      */
     void dataReady(QString mydata);
 
+    /**
+     * @brief This signal is emitted when the requested location was retrieved
+     * from the API. In case there was no matching location (i.e. the API does not
+     * provide weather data about the requested location) then @a locations will
+     * be empty.
+     *
+     * @param locations A list of all locations matching the request given in
+     * @a requestLocation. Empty if there was no match.
+     */
+    void locationsReady(QStringList locations);
+
 public slots:
     /**
      * @brief Request new data by issueing a call to the weather API.
      */
     void requestData();
+
+    /**
+     * @brief Request all locations provided by the weather API that are near
+     * the @a desiredLocation
+     *
+     * @param desiredLocation The location that was requested by the user
+     */
+    void requestLocation(const QString& desiredLocation);
+
+private slots:
+    /**
+     * @brief Clean up after each call to the weather API
+     */
+    void cleanUp();
 
 private:
     std::unique_ptr<QNetworkAccessManager> m_NetAccessManager{};
