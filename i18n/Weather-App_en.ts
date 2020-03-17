807b199da8fc2bc3f91bead783391d13c0640e14
Florian Meinicke <florian.meinicke@t-online.de>
2020-03-17 11:43:52 +0100

    Add translation for weather state names from the API
    by using a helper map. It maps the english states from the API to the
    translated strings in each language

--- i18n/Weather-App_en.ts
+++ i18n/Weather-App_en.ts
@@ -4,45 +4,98 @@
 <context>
     <name>AboutPage</name>
     <message>
-        <location filename="../qml/AboutPage.qml" line="14"/>
+        <location filename="../res/qml/AboutPage.qml" line="14"/>
         <source>About</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="38"/>
+        <location filename="../res/qml/AboutPage.qml" line="38"/>
         <source>Version %1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="58"/>
+        <location filename="../res/qml/AboutPage.qml" line="58"/>
         <source>Copyright (c) 2020 Florian Meinicke</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="68"/>
+        <location filename="../res/qml/AboutPage.qml" line="68"/>
         <source>&lt;html&gt;Licensed under &lt;a href=&apos;https://opensource.org/licenses/MIT&apos;&gt;MIT License&lt;/a&gt;&lt;/html&gt;</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="90"/>
+        <location filename="../res/qml/AboutPage.qml" line="90"/>
         <source>Credits and Icon Sources</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="99"/>
+        <location filename="../res/qml/AboutPage.qml" line="99"/>
         <source>&lt;html&gt;This app uses the free API available at &lt;a href=&apos;https://www.metaweather.com&apos;&gt;MetaWeather.com&lt;/a&gt;. All weather icons as well as the app icon are taken from there.&lt;/html&gt;</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="114"/>
+        <location filename="../res/qml/AboutPage.qml" line="114"/>
         <source>&lt;html&gt;Material Icons: Downloaded from &lt;a href=&apos;https://www.material.io/resources/icons/&apos;&gt;material.io&lt;/a&gt;&lt;/html&gt;</source>
         <translation></translation>
     </message>
 </context>
 <context>
+    <name>CWeatherDataModel</name>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="172"/>
+        <source>Snow</source>
+        <translation>Snow</translation>
+    </message>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="173"/>
+        <source>Sleet</source>
+        <translation>Sleet</translation>
+    </message>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="174"/>
+        <source>Hail</source>
+        <translation>Hail</translation>
+    </message>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="175"/>
+        <source>Thunderstorm</source>
+        <translation>Thunderstorm</translation>
+    </message>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="176"/>
+        <source>Heavy Rain</source>
+        <translation>Heavy Rain</translation>
+    </message>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="177"/>
+        <source>Light Rain</source>
+        <translation>Light Rain</translation>
+    </message>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="178"/>
+        <source>Showers</source>
+        <translation>Showers</translation>
+    </message>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="179"/>
+        <source>Heavy Cloud</source>
+        <translation>Heavy Cloud</translation>
+    </message>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="180"/>
+        <source>Light Cloud</source>
+        <translation>Light Cloud</translation>
+    </message>
+    <message>
+        <location filename="../src/WeatherDataModel.cpp" line="181"/>
+        <source>Clear</source>
+        <translation>Clear</translation>
+    </message>
+</context>
+<context>
     <name>SearchPageForm</name>
     <message>
-        <location filename="../qml/SearchPageForm.qml" line="16"/>
+        <location filename="../res/qml/SearchPageForm.qml" line="16"/>
         <source>Weather Forecast</source>
         <translation></translation>
     </message>
@@ -50,58 +103,58 @@
 <context>
     <name>WeatherDataDelegate</name>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="48"/>
-        <location filename="../qml/WeatherDataDelegate.qml" line="219"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="48"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="219"/>
         <source>Full forecast</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="59"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="59"/>
         <source>Show less</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="99"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="99"/>
         <source>%1 °C</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="128"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="128"/>
         <source>Min: %1 °C</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="136"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="136"/>
         <source>Max: %1 °C</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="150"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="150"/>
         <source>Wind: %1 km/h</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="170"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="170"/>
         <source>Air Pressure: %1 hPa</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="179"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="179"/>
         <source>Humidity: %1%</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="189"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="189"/>
         <source>Visibility: %1 km</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="199"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="199"/>
         <source>Sunrise: %1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherDataDelegate.qml" line="209"/>
+        <location filename="../res/qml/WeatherDataDelegate.qml" line="209"/>
         <source>Sunset: %1</source>
         <translation></translation>
     </message>
@@ -109,22 +162,22 @@
 <context>
     <name>WeatherForecastPageForm</name>
     <message>
-        <location filename="../qml/WeatherForecastPageForm.qml" line="71"/>
+        <location filename="../res/qml/WeatherForecastPageForm.qml" line="71"/>
         <source>Today</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherForecastPageForm.qml" line="73"/>
+        <location filename="../res/qml/WeatherForecastPageForm.qml" line="73"/>
         <source>Tomorrow</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherForecastPageForm.qml" line="118"/>
+        <location filename="../res/qml/WeatherForecastPageForm.qml" line="118"/>
         <source>Tap the search icon above to search a location.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/WeatherForecastPageForm.qml" line="130"/>
+        <location filename="../res/qml/WeatherForecastPageForm.qml" line="130"/>
         <source>Weather Forecast</source>
         <translation></translation>
     </message>
@@ -132,43 +185,43 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="103"/>
+        <location filename="../res/qml/main.qml" line="106"/>
         <source>Add Location to Favourites</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="106"/>
+        <location filename="../res/qml/main.qml" line="109"/>
         <source>Added %1 to Favourites</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="128"/>
+        <location filename="../res/qml/main.qml" line="131"/>
         <source>Remove Location from Favourites</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="131"/>
+        <location filename="../res/qml/main.qml" line="134"/>
         <source>Removed %1 from Favourites</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="159"/>
-        <location filename="../qml/main.qml" line="163"/>
+        <location filename="../res/qml/main.qml" line="160"/>
+        <location filename="../res/qml/main.qml" line="164"/>
         <source>About</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="193"/>
+        <location filename="../res/qml/main.qml" line="194"/>
         <source>Favourite Locations</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="252"/>
+        <location filename="../res/qml/main.qml" line="253"/>
         <source>Press again to quit...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="320"/>
+        <location filename="../res/qml/main.qml" line="321"/>
         <source>Undo</source>
         <translation></translation>
     </message>
